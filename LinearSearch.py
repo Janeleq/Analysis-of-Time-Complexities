9b04d972aeaed322cbe4c66230de23b711c3e4d1
Janeleq <janelim2001@gmail.com>
2022-09-12 12:47:25 +0800

    first commit

---
+++ LinearSearch.py
@@ -0,0 +1,29 @@
+import random, time
+
+#Linear Search
+#Best case: 1, Worst Case: n, Average = (n-1)/2
+#Big O: O(n) = n
+
+#using while loop
+def lSearch1(array, key):
+    i = 0
+    while i < len(array):
+        if array[i] == key:
+            return i
+        i += 1
+    return -1
+
+#using for loop
+def lSearch2(array, key):
+    i = 0
+    for i in range(len(array)):
+        if array[i] == key:
+            return i
+    return -1
+
+print("Starting Timer...")
+start_time = time.time()
+output = lSearch1([1,2,3,8,9], 9)
+time_taken = time.time() - start_time
+print(f"Execution Time: {time_taken}")
+# print(f"Index Position: {output}")``
