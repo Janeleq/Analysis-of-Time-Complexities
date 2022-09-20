807407ccd9066db4a2094b6667051f6eb41c9689
Jane lim <janelim2001@gmail.com>
2022-09-21 01:19:00 +0800

    Update Changes

--- Recursive/BinarySearch.py
+++ Recursive/BinarySearch.py
@@ -19,3 +19,20 @@ def rbsearch(array, target, lower=None, upper=None):
 
 output = rbsearch([1,4,3,88,38], 1)
 print(output)
+
+# Another binary implementation with a tweak
+def rbsearch(array, target, lower=None, upper=None):
+    if lower == None:
+        lower = - 1
+        upper = len(array)
+
+    if lower > upper: # base case
+        return -1           # not found
+
+    mid = (lower + upper)//2
+    if array[mid] == target: # base case
+        return mid          # success
+    elif array[mid] < target: # search upper region
+        return rbsearch(array, target, mid-1, upper)
+    else:                      # search lower region 
+        return rbsearch(array, target, lower, mid+1)
--- Recursive/Factorial.py
+++ Recursive/Factorial.py
@@ -1,7 +1,12 @@
 # Reduction step = factorial(n) = n x factorial(n-1)
 # Base case = factorial(1) = 1
+
+# Time complexity: O(n)
+
+# Space complexity: O(n)
+# Max depth of recursion calls = n
 def factorial(n):
-    if n == 1:
+    if n == 1: # 1 operation; O(n)
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n-1) 
