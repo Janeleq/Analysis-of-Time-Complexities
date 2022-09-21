ea759adfc2f02e6fb23fdd6103952d4731c6cf85
Jane lim <janelim2001@gmail.com>
2022-09-21 11:21:35 +0800

    Create dijkstra.py

---
+++ LDS/dijkstra.py
@@ -0,0 +1,32 @@
+# Recursive Implementation of Dijkstra Algorithm
+
+def dijkstra(a,b):
+    if a == b:
+        return a
+    if a > b:
+        return dijkstra(a-b, b)
+    else:
+        return dijkstra(a, b-a)
+
+# Stack Implementation of Dijkstra Algorithm
+
+def stk_dijkstra(a,b):
+    stk = Stack()
+    stk.push(a)
+    stk.push(b)
+    while stk.count() > 0:
+        b = stk.pop()
+        a = stk.pop()
+
+        if a == b:
+            return a
+
+        elif a > b:
+            stk.push(a-b)
+            stk.push(b)
+
+        else:
+            stk.push(a)
+            stk.push(b-a)
+
+        
