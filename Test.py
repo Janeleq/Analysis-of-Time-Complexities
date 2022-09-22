6c93d8a478a395794d97c8ee619cb4ebade520a3
Jane lim <janelim2001@gmail.com>
2022-09-22 14:57:37 +0800

    Update Changes

---
+++ Test.py
@@ -0,0 +1,8 @@
+# multiple classes so have to access the file itself, cannot access through folder
+from AbstractDataTypes.BinaryTree import BinarySearchTree, BinaryTree, Node, TreeUpdater
+from LinearDataStructures.Stacks import Queue
+
+
+tree1 = BinaryTree()
+
+
